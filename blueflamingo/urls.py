d31706fab9c13a92ac27b10fc1f92eef3e14bcb5
daprieu <daprieu@gmail.com>
2021-06-11 09:40:10 -0500

    created tokens and added Auth views so a user can login/out/regitster

--- blueflamingo/urls.py
+++ blueflamingo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blueflamingoapi.views import register_user, login_user
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 ]
